ba6c12025c23f9a98300222e3bfba67045106fb9
Matt Maclean <mwmaclean88@gmail.com>
2017-01-06 12:47:18 +0000

    Removed unused font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,7 +14,6 @@
 
 	<!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
